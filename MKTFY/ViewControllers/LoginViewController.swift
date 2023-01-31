e8f2a88ee911ceecc6982f8db969629abd4d613e
[treasure3210] <treasure3210@gmail.com>
2023-01-30 22:46:33 -0700

    Connect IBOutlets and IBActions

--- MKTFY/ViewControllers/LoginViewController.swift
+++ MKTFY/ViewControllers/LoginViewController.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 class LoginViewController: ViewController {
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var cloudsImageView: UIImageView!
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    @IBAction func createAccountButton(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
