dfd438c19875c942ae418a389a619a742a9efdfd
[treasure3210] <treasure3210@gmail.com>
2023-01-30 22:56:05 -0700

    Fixed bad connection of IB Reference Outlets

--- MKTFY/ViewControllers/MainController/MainViewController.swift
+++ MKTFY/ViewControllers/MainController/MainViewController.swift
@@ -14,7 +14,22 @@ class MainViewController: ViewController {
         // Do any additional setup after loading the view.
         
         let vc = LoginViewController.storyboardInstance(storyboardName: "LoginScreen")
-        UIApplication.shared.windows.first?.rootViewController = vc
+        UIApplication.shared.currentUIWindow()?.rootViewController = vc
     }
     
+}
+
+public extension UIApplication {
+    func currentUIWindow() -> UIWindow? {
+        let connectedScenes = UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .compactMap { $0 as? UIWindowScene }
+        
+        let window = connectedScenes.first?
+            .windows
+            .first { $0.isKeyWindow }
+
+        return window
+        
+    }
 }
