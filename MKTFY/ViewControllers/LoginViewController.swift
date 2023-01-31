8ce6a56e126ff859fba4e6e2cae6178c418acc68
[treasure3210] <treasure3210@gmail.com>
2023-01-31 11:06:54 -0700

    Implement dismiss keyboard under 2 conditions - Tapping anywhere from the screen using gesture detector : requires setting up an extension with a method initializeHideKeyboard. - Tapping "return" : requires the use of self.view.endEditing(true) to the textFieldShouldReturn from the Extension, then implementing self.emailTextField.delegate = self or self.passwordTextField.delegate = self

--- MKTFY/ViewControllers/LoginViewController.swift
+++ MKTFY/ViewControllers/LoginViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class LoginViewController: ViewController {
+class LoginViewController: UIViewController {
     @IBOutlet weak var titleImageView: UIImageView!
     @IBOutlet weak var subTitleLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
+    let errorMessageLabel = UILabel()
     
     @IBAction func forgotPasswordButton(_ sender: Any) {
     }
@@ -28,8 +29,72 @@ class LoginViewController: ViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        initializeHideKeyboard()
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
     }
     
 }
+
+extension LoginViewController {
+    
+    @objc func loginButton(sender: UIButton) {
+        login()
+    }
+    
+    private func login() {
+        guard let emailTextField = emailTextField, let passwordTextField = passwordTextField else {
+            assertionFailure("Email and/or password should never be nil.")
+            return
+        }
+        
+        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
+            configureView(withMessage: "Username and/or password cannot be blank")
+            return
+        }
+        
+        if emailTextField.text == "" && passwordTextField.text == "" {
+            
+        } else {
+            configureView(withMessage: "Incorrect Username and/or password")
+        }
+        
+    }
+    
+    private func configureView(withMessage message: String){
+        errorMessageLabel.isHidden = false
+        errorMessageLabel.text = message
+    }
+}
+
+// Enable dismiss of keyboard when the user taps anywhere from the screen
+extension LoginViewController {
+    func initializeHideKeyboard(){
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissMyKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissMyKeyboard(){
+        view.endEditing(true)
+    }
+}
+
+// Enable dismiss of keyboard when the user taps "return"
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+}
+
