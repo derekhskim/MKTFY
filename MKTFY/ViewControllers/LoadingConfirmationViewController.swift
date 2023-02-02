d4c8659783a6f6529510f0e32412fd0ff6e5e122
[treasure3210] <treasure3210@gmail.com>
2023-02-02 11:16:15 -0700

    Setup automatic transition of the screen (View Controller -> Animation Page -> View Controller)

--- MKTFY/ViewControllers/LoadingConfirmationViewController.swift
+++ MKTFY/ViewControllers/LoadingConfirmationViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 class LoadingConfirmationViewcontroller: UIViewController {
     @IBOutlet weak var navItem: UINavigationItem!
     
+    @IBOutlet weak var circularIndicator: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        circularIndicator?.animationRepeatCount = 1
+        circularIndicator?.startAnimating()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + circularIndicator.animationDuration) {
+            let forgotPasswordVerificationViewController = self.storyboard?.instantiateViewController(withIdentifier: "ForgotPasswordVerificationViewController")
+            self.show(forgotPasswordVerificationViewController!, sender: nil)
+        }
     }
 }
