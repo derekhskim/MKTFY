c4ae50047b86702755b3fd836860509c23775498
[treasure3210] <treasure3210@gmail.com>
2023-02-10 13:19:41 -0700

    Add extension to allow view to shift up and down when textfields are selected Add extension to dismiss keyboard with gesture & return

--- MKTFY/ViewControllers/CreateAccountViewController.swift
+++ MKTFY/ViewControllers/CreateAccountViewController.swift
@@ -11,8 +11,24 @@ class CreateAccountViewController: UIViewController {
 
     @IBOutlet weak var backgroundView: UIView!
     
+    @IBOutlet weak var firstNameField: TextFieldWithError!
+    @IBOutlet weak var lastNameField: TextFieldWithError!
+    @IBOutlet weak var emailField: TextFieldWithError!
+    @IBOutlet weak var phoneField: TextFieldWithError!
+    @IBOutlet weak var addressField: TextFieldWithError!
+    @IBOutlet weak var cityField: TextFieldWithError!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initializeHideKeyboard()
+        
+        self.firstNameField.inputTextField.delegate = self
+        self.lastNameField.inputTextField.delegate = self
+        self.emailField.inputTextField.delegate = self
+        self.phoneField.inputTextField.delegate = self
+        self.addressField.inputTextField.delegate = self
+        self.cityField.inputTextField.delegate = self
         
         // Controls the back button's action and style
         let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonTapped))
@@ -30,5 +46,46 @@ class CreateAccountViewController: UIViewController {
 extension CreateAccountViewController {
     @objc func backButtonTapped() {
         self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension CreateAccountViewController {
+    @objc func keyboardWillShow(sender: NSNotification) {
+         self.view.frame.origin.y = -100 // Move view 100 points upward
+    }
+
+    @objc func keyboardWillHide(sender: NSNotification) {
+         self.view.frame.origin.y = 0 // Move view to original position
+    }
+}
+
+// Enable dismiss of keyboard when the user taps anywhere from the screen
+extension CreateAccountViewController {
+    func initializeHideKeyboard(){
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissMyKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissMyKeyboard(){
+        view.endEditing(true)
+    }
+}
+
+// Enable dismiss of keyboard when the user taps "return"
+extension CreateAccountViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
     }
 }
