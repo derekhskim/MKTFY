9d865b34b35c004ebe3be19ea449db25222fa2ef
[treasure3210] <treasure3210@gmail.com>
2023-02-17 13:50:50 -0700

    Deletion of unused code enabled email - password less connection from database verification is now being sent via email from Auth0

--- MKTFY/Models/Auth0Manager.swift
+++ MKTFY/Models/Auth0Manager.swift
@@ -14,7 +14,7 @@ class Auth0Manager {
     
     
     func loginWithEmail(_ email: String, password: String, completion: @escaping (Bool, Error?) -> Void) {
-                
+        
         auth0.login(
             usernameOrEmail: email,
             password: password,
@@ -36,23 +36,23 @@ class Auth0Manager {
     
     func signup(email: String, password: String, firstName: String, lastName: String, phone: String, address: String, city: String, completion: @escaping (Bool, Error?) -> Void) {
         
-            let userMetadata = ["firstName" : firstName, "lastName" : lastName, "email" : email, "phone" : phone, "address" : address, "city" : city]
-            auth0.signup(email: email, password: password, connection: "Username-Password-Authentication", userMetadata: userMetadata)
-                .start { result in
-                    switch result {
-                    case .success(let user):
-                        print("User signed up: \(user)")
-                        completion(true, nil)
-                    case .failure(let error):
-                        print("Failed to sign up: \(error)")
-                        completion(false, error)
-                    }
+        let userMetadata = ["firstName" : firstName, "lastName" : lastName, "email" : email, "phone" : phone, "address" : address, "city" : city]
+        auth0.signup(email: email, password: password, connection: "Username-Password-Authentication", userMetadata: userMetadata)
+            .start { result in
+                switch result {
+                case .success(let user):
+                    print("User signed up: \(user)")
+                    completion(true, nil)
+                case .failure(let error):
+                    print("Failed to sign up: \(error)")
+                    completion(false, error)
                 }
-        }
+            }
+    }
     
     func resetPassword(email: String) {
         
-        auth0.startPasswordless(email: email, type: .code, connection: "Username-Password-Authentication")
+        auth0.startPasswordless(email: email, type: .code, connection: "email")
             .start { result in
                 switch result {
                 case .success:
@@ -61,17 +61,7 @@ class Auth0Manager {
                     print("Failed to send reset password email: \(error.localizedDescription)")
                 }
             }
-        
-//        auth0.resetPassword(email: email, connection: "Username-Password-Authentication")
-//            .start { result in
-//                switch result {
-//                case .success:
-//                    print("Reset password email sent successfully")
-//                case .failure(let error):
-//                    print("Failed to send reset password email: \(error.localizedDescription)")
-//                }
-//            }
     }
-
+    
     
 }
