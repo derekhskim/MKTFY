d724e7def57481ed669a75e38124d4d65caf4c7c
[treasure3210] <treasure3210@gmail.com>
2023-02-21 11:20:21 -0700

    hide navigation controller from LoadingConfirmationViewController

--- MKTFY/ViewControllers/LoadingViewController/LoadingConfirmationViewController.swift
+++ MKTFY/ViewControllers/LoadingViewController/LoadingConfirmationViewController.swift
@@ -16,26 +16,9 @@ class LoadingConfirmationViewcontroller: UIViewController {
         
         /// MARK: - Confirmation Check Animation Lottie GIF file
         imageView.image = UIImage.animatedGif(named: "confirmation_check")
-                
-//        imageView?.animationRepeatCount = 1
-//        imageView?.startAnimating()
-//        
-//        /// MARK: - Automatically transitions to the next view after 2.2 seconds, which is the time when animation completes
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.2) {
-//            let forgotPasswordVerificationViewController = self.storyboard?.instantiateViewController(withIdentifier: "ForgotPasswordVerificationViewController")
-//            self.show(forgotPasswordVerificationViewController!, sender: nil)
+        
+        navigationController?.setNavigationBarHidden(true, animated: true)
+
         }
     }
     
-    
-//    /// MARK: - Controls whether if you want to hide or show the navigation bar
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        navigationController?.setNavigationBarHidden(true, animated: animated)
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        navigationController?.setNavigationBarHidden(false, animated: animated)
-//    }
-//}
