30a65acc4a9f8189a1cf4e870cd9fa891879dcd5
[treasure3210] <treasure3210@gmail.com>
2023-02-21 23:17:06 -0700

    Add default/simple style for ResetPasswordViewController

--- MKTFY/ViewControllers/ResetPasswordViewController.swift
+++ MKTFY/ViewControllers/ResetPasswordViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ResetPasswordViewController: UIViewController {
     
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationBar()
+        setupBackgroundView(view: backgroundView)
     }
     
     
 }
+
+// Determines where the back button should take the view controller to
+extension ResetPasswordViewController {
+    @objc override func backButtonTapped() {
+        self.navigationController?.popToViewController(self.navigationController!.children[1], animated: true)
+    }
+}
+
