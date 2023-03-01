9bc998bb446392b9d6f33caec0ca5f863caa7480
[treasure3210] <treasure3210@gmail.com>
2023-03-01 11:05:46 -0700

    Create TermsOfServiceVC page with scrollView VC setup

--- MKTFY/ViewControllers/MainController/PrivacyPolicyViewController.swift
+++ MKTFY/ViewControllers/MainController/PrivacyPolicyViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class PrivacyPolicyViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
+        setupBackgroundView(view: backgroundView)
     }
 }
--- MKTFY/ViewControllers/TermsOfServiceViewController.swift
+++ MKTFY/ViewControllers/TermsOfServiceViewController.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
-class TermsOfServiceViewController: UIViewController {
-
+class TermsOfServiceViewController: UIViewController, LoginStoryboard {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBAction func acceptButtonTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+        setupBackgroundView(view: backgroundView)
         
     }
 }
