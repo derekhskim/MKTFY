d64caeab601b1f8b19ff914876863142d4c37b78
[treasure3210] <treasure3210@gmail.com>
2023-03-01 10:16:20 -0700

    create TermsOfService & PrivacyPolicy view controllers from storyboard and file

---
+++ MKTFY/ViewControllers/MainController/PrivacyPolicyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyPolicyViewController.swift
+//  MKTFY
+//
+//  Created by Derek Kim on 2023/03/01.
+//
+
+import UIKit
+
+class PrivacyPolicyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ MKTFY/ViewControllers/TermsOfServiceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TermsOfServiceViewController.swift
+//  MKTFY
+//
+//  Created by Derek Kim on 2023/03/01.
+//
+
+import UIKit
+
+class TermsOfServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
