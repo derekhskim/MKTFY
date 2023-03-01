835a2c01bb1dd097f5b581f8873cc3aed3a1360c
[treasure3210] <treasure3210@gmail.com>
2023-02-28 21:43:35 -0700

    Fix code for ambiguity

--- MKTFY/ViewControllers/ForgotPasswordVerificationViewController.swift
+++ MKTFY/ViewControllers/ForgotPasswordVerificationViewController.swift
@@ -10,10 +10,10 @@ import Auth0
 
 class ForgotPasswordVerificationViewController: UIViewController {
     
+    let auth0Manager = Auth0Manager()
     var email: String!
     var mgmtAccessToken: String = ""
     var userId: String = ""
-    let audience = "https://\(domain)/api/v2/"
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var verificationTextField: TextFieldWithError!
@@ -37,7 +37,7 @@ class ForgotPasswordVerificationViewController: UIViewController {
                 
                 let headers = ["authorization": "Bearer \(self.mgmtAccessToken)"]
                 
-                let request = NSMutableURLRequest(url: NSURL(string: "\(audience)users?q=email:%22\(email)%22&search_engine=v3")! as URL,
+                let request = NSMutableURLRequest(url: NSURL(string: "https://\(domain)/api/v2/users?q=email:%22\(email)%22&search_engine=v3")! as URL,
                                                   cachePolicy: .useProtocolCachePolicy,
                                                   timeoutInterval: 10.0)
                 request.httpMethod = "GET"
@@ -61,7 +61,7 @@ class ForgotPasswordVerificationViewController: UIViewController {
                                                             self.userId = user_id
                                                             UserDefaults.standard.set(self.userId, forKey: "user_id")
                                                             
-                                                            self.Auth0Manager.shared.login(email: email, code: cleanVerificationCode, audience: audience, scope: "openid profile")
+                                                            self.auth0Manager.auth0.login(email: email, code: cleanVerificationCode, audience: "https://\(domain)/api/v2/", scope: "openid profile")
                                                                 .start { result in
                                                                     switch result {
                                                                     case .success(_):
