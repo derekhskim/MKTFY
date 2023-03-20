c7eaae36d75459cef9a6f1a5b564ccd429d92822
[treasure3210] <treasure3210@gmail.com>
2023-03-20 13:32:19 -0600

    Create custom view for NotificationViewTile to avoid repetitive addition of views for notificationVC

---
+++ MKTFY/Views/NotificationViewTile/NotificationViewTile.swift
@@ -0,0 +1,41 @@
+//
+//  NotificationViewTile.swift
+//  MKTFY
+//
+//  Created by Derek Kim on 2023-03-20.
+//
+
+import UIKit
+
+@IBDesignable
+class NotificationViewTile: LPView {
+    
+    var view: UIView!
+    
+    
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "ListingViewTile", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+    
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth, UIView.AutoresizingMask.flexibleHeight]
+        addSubview(view)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+}
+
