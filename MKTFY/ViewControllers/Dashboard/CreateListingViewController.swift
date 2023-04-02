c3b8cec16015b0b1dcc2c42871814fee14a726f8
[treasure3210] <treasure3210@gmail.com>
2023-04-02 01:12:23 -0600

    Add xButton and tapRecognizer Add TODO

--- MKTFY/ViewControllers/Dashboard/CreateListingViewController.swift
+++ MKTFY/ViewControllers/Dashboard/CreateListingViewController.swift
@@ -14,35 +14,57 @@ class CreateListingViewController: MainViewController, DashboardStoryboard, UIIm
     
     // MARK: - @IBAction
     @IBAction func uploadImageButtonTapped(_ sender: Any) {
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.delegate = self
-        imagePickerController.sourceType = .photoLibrary
-        imagePickerController.allowsEditing = false
-        imagePickerController.mediaTypes = ["public.image"]
-        if #available(iOS 14, *) {
-            imagePickerController.modalPresentationStyle = .fullScreen
+        //        let imagePickerController = UIImagePickerController()
+        //        imagePickerController.delegate = self
+        //        imagePickerController.sourceType = .photoLibrary
+        //        imagePickerController.allowsEditing = false
+        //        imagePickerController.mediaTypes = ["public.image"]
+        //        if #available(iOS 14, *) {
+        //            imagePickerController.modalPresentationStyle = .fullScreen
+        //        }
+        //
+        //        present(imagePickerController, animated: true, completion: nil)
+        //
+        //        print("Tapped")
+        
+        if uploadImageButton.chosenImages.isEmpty {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = .photoLibrary
+            imagePickerController.allowsEditing = false
+            imagePickerController.mediaTypes = ["public.image"]
+            if #available(iOS 14, *) {
+                imagePickerController.modalPresentationStyle = .fullScreen
+            }
+            
+            present(imagePickerController, animated: true, completion: nil)
+            
+            print("Tapped")
+        } else {
+            // Remove the image if it's already set
+            print("Remove Image button tapped")
+            if !uploadImageButton.chosenImages.isEmpty {
+                uploadImageButton.chosenImages.removeFirst()
+                uploadImageButton.updateAppearance()
+            }
         }
-        
-        present(imagePickerController, animated: true, completion: nil)
-        
-        print("Tapped")
     }
     
     // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-            if let image = info[.originalImage] as? UIImage, uploadImageButton.chosenImages.count < 3 {
-                uploadImageButton.chosenImages.append(image)
-            }
-            picker.dismiss(animated: true, completion: nil)
+        if let image = info[.originalImage] as? UIImage, uploadImageButton.chosenImages.count < 3 {
+            uploadImageButton.chosenImages.append(image)
         }
-
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            picker.dismiss(animated: true, completion: nil)
-        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
--- MKTFY/Views/CustomButton/UploadImageButton.swift
+++ MKTFY/Views/CustomButton/UploadImageButton.swift
@@ -17,20 +17,25 @@ class UploadImageButton: UIButton {
     private let mainImageView = UIImageView()
     private let addImageIcon = UIImageView()
     private let instructionLabel = UILabel()
+    private let xButton = UIImageView()
     
-    private func updateAppearance() {
+    // TODO: Only enable xButton to remove photos
+    // TODO: Add subviews beneath for additional images view and add more photo up to 3 photos and disappear
+    // TODO: allow multiple selection of photo
+    func updateAppearance() {
         print("Updating appearance with \(chosenImages.count) image(s)")
         
         if let firstImage = chosenImages.first {
             mainImageView.image = firstImage
             addImageIcon.isHidden = true
             instructionLabel.isHidden = true
-            isUserInteractionEnabled = false
+            xButton.isHidden = false
             layer.borderWidth = 0
         } else {
             mainImageView.image = nil
             addImageIcon.isHidden = false
             instructionLabel.isHidden = false
+            xButton.isHidden = true
             setTitle("Choose up to 3 photos", for: .normal)
             isUserInteractionEnabled = true
             layer.borderWidth = 1
@@ -50,15 +55,18 @@ class UploadImageButton: UIButton {
     private func setup() {
         layer.borderColor = UIColor.appColor(LPColor.OccasionalPurple).cgColor
         layer.borderWidth = 1
+        layer.cornerRadius = 10
         setTitleColor(.clear, for: .normal)
         titleLabel?.textAlignment = .center
         
         addSubview(mainImageView)
         addSubview(addImageIcon)
         addSubview(instructionLabel)
+        addSubview(xButton)
         
         mainImageView.translatesAutoresizingMaskIntoConstraints = false
         mainImageView.contentMode = .scaleAspectFill
+        mainImageView.isUserInteractionEnabled = false
         mainImageView.clipsToBounds = true
         
         addImageIcon.image = UIImage(named: "add_image_camera")
@@ -66,9 +74,18 @@ class UploadImageButton: UIButton {
         addImageIcon.contentMode = .center
         
         instructionLabel.text = "Choose up to 3 photos"
+        instructionLabel.font = UIFont(name: "OpenSans-Regular", size: 14)
         instructionLabel.textAlignment = .center
         instructionLabel.textColor = UIColor.appColor(LPColor.OccasionalPurple)
         instructionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        xButton.image = UIImage(systemName: "x.circle.fill")
+        xButton.tintColor = UIColor.appColor(LPColor.MistakeRed)
+        xButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(removeImage))
+        xButton.isUserInteractionEnabled = true
+        xButton.addGestureRecognizer(tapGestureRecognizer)
         
         NSLayoutConstraint.activate([
             mainImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
@@ -83,9 +100,23 @@ class UploadImageButton: UIButton {
             
             instructionLabel.topAnchor.constraint(equalTo: addImageIcon.bottomAnchor, constant: 13),
             instructionLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            instructionLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
+            instructionLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            xButton.topAnchor.constraint(equalTo: topAnchor, constant: 11),
+            xButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11),
+            xButton.heightAnchor.constraint(equalToConstant: 21),
+            xButton.widthAnchor.constraint(equalToConstant: 21)
         ])
         
         updateAppearance()
+    }
+    
+    @objc private func removeImage() {
+        print("remove Image button tapped")
+        
+        if !chosenImages.isEmpty {
+            chosenImages.removeFirst()
+            updateAppearance()
+        }
     }
 }
